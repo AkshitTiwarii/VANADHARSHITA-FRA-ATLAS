af25d35f7774d531cdef815a170549fef32f80b7
AkshitTiwarii <akshittiwari29@gmail.com>
2025-09-26 01:35:20 +0530

    Set LandingPage as default home page with 'Get Started' button
     Updated routing to show LandingPage on '/' route
     Changed 'Access Portal' buttons to 'Get Started'
     'Get Started' button now navigates to login/signup page
     Added LandingPage import to App.js routing
     Users now see professional landing page first, then can access login

--- frontend-main/src/components/LandingPage.js
+++ frontend-main/src/components/LandingPage.js
@@ -130,7 +130,7 @@ const LandingPage = () => {
               <Link to="/login">
                 <Button size="lg" className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-3">
                   <Play className="w-5 h-5 mr-2" />
-                  Access Portal
+                  Get Started
                   <ArrowRight className="w-5 h-5 ml-2" />
                 </Button>
               </Link>
@@ -274,7 +274,7 @@ const LandingPage = () => {
             <Link to="/login">
               <Button size="lg" className="bg-orange-500 hover:bg-orange-600 px-8 py-3">
                 <Shield className="w-5 h-5 mr-2" />
-                Access Secure Portal
+                Get Started
                 <ArrowRight className="w-5 h-5 ml-2" />
               </Button>
             </Link>
